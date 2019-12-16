5944066394b81d8d8fe2cdbdb03f195b65d85327
Vladimir Kuznetsov <smithcoder@yandex.ru>
2019-12-16 23:21:24 +0300

    Многостроковая секция код

--- markdown2html.cpp
+++ markdown2html.cpp
@@ -19,6 +19,18 @@ QString markdown2html(const QString& source)
 	{
 		QChar ch = source.at(i);
 
+		if (wait == Code && section == Code)
+		{
+			wait = None;
+			if (ch == "\t")
+			{
+				res.append("<br/>\n");
+				continue;
+			}
+			res.append("</code>\n");
+			section = None;
+		}
+
 		if (section == None)
 		{
 			if (ch == "#")
@@ -30,6 +42,7 @@ QString markdown2html(const QString& source)
 			{
 				section = Code;
 				res.append("<code>");
+				wait = None;
 			}
 			else if (ch == " " && wait == Header)
 			{
@@ -49,16 +62,22 @@ QString markdown2html(const QString& source)
 		else if (ch == "\n")
 		{
 			if (section == Text)
+			{
 				res.append("</p>\n");
+				section = None;
+			}
 			else if (section == Code)
-				res.append("</code>\n");
+			{
+				wait = Code;
+			}
 			else if (section == Header)
 			{
 				res.append("</h" % QString::number(level) % ">\n");
 				level = 0;
+				section = None;
 			}
 
-			section = None;
+
 		}
 		else
 			res.append(ch);
